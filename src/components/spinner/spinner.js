80e70ebcaa347e9aae32f5b0d5d390ac8dc7dbd6
Dmitry Mikhaylov <dmikhaylov7@gmail.com>
2020-07-06 20:51:31 +0300

    randomChar from API

---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './spinner.css';
+
+const Spinner = () => {
+    return (
+        <div className="loadingio-spinner-rolling-t45btfbl2i"><div className="ldio-b66w71cnou">
+        <div></div>
+        </div></div>
+    )
+}
+export default Spinner;
+
+
